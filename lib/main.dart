5b010adff6d5cb317b7ec7b42582709eae26c70d
Johnny Vanhentenryk <johnny.vanhentenryk@ixor.be>
2022-12-23 00:54:23 +0100

    Add form validation

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        //home: ContactCreatePage(),
-        home: ContactsListPage(),
+        home: ContactCreatePage(),
+        //home: ContactsListPage(),
       ),
     );
 
--- lib/ui/contact/widget/contact_form.dart
+++ lib/ui/contact/widget/contact_form.dart
@@ -7,7 +7,8 @@ class ContactForm extends StatefulWidget {
 class _ContactFormState extends State<ContactForm> {
   final _formKey = GlobalKey<FormState>();
 
-  String? _name = '';
+  String? _name;
+
   String? _email = '';
   String? _phoneNumber = '';
 
@@ -17,18 +18,80 @@ class _ContactFormState extends State<ContactForm> {
       key: _formKey,
       child: ListView(
         children: <Widget>[
-          TextFormField(onSaved: (newValue) => _name = newValue),
-          TextFormField(onSaved: (newValue) => _email = newValue),
-          TextFormField(onSaved: (newValue) => _phoneNumber = newValue),
+          SizedBox(height: 10),
+          TextFormField(
+            onSaved: (newValue) => _name = newValue,
+            validator: _validateName,
+            decoration: InputDecoration(
+                labelText: 'Name',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            onSaved: (newValue) => _email = newValue,
+            validator: _validateEmail,
+            decoration: InputDecoration(
+                labelText: 'Email',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            onSaved: (newValue) => _phoneNumber = newValue,
+            validator: _validatePhoneNumber,
+            decoration: InputDecoration(
+                labelText: 'Phone Number',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
+          ),
+          SizedBox(height: 10),
           ElevatedButton(
+            //style: ElevatedButton.styleFrom(foregroundColor: Colors.black),
             onPressed: () {
-              _formKey.currentState?.save();
-              print(_name! + ' ' + _email! + ' ' + _phoneNumber!);
+              if (true == _formKey.currentState?.validate()) {
+                _formKey.currentState?.save();
+                print('Saved: ' + _name! + ' ' + _email! + ' ' + _phoneNumber!);
+              }
             },
-            child: Text('Save'),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Save'),
+                Icon(
+                  Icons.person,
+                  size: 18,
+                )
+              ],
+            ),
           ),
         ],
       ),
     );
+  }
+
+  String? _validateName(String? name) {
+    if (name == null) return 'Enter a name';
+    if (name.isEmpty) return 'Enter a name';
+
+    return null;
+  }
+
+  String? _validateEmail(String? email) {
+    if (email == null) return 'Enter a email';
+    if (email.isEmpty) return 'Enter a email';
+    var emailRegex = RegExp( r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+    if (!emailRegex.hasMatch(email)) return 'Invalid email';
+
+    return null;
+  }
+
+  String? _validatePhoneNumber(String? nr) {
+    if (nr == null) return 'Enter a PhoneNumber';
+    if (nr.isEmpty) return 'Enter a PhoneNumber';
+    var phoneRegex = RegExp( r"^\+(?:[0-9] ?){6,14}[0-9]$");
+    if (!phoneRegex.hasMatch(nr)) return 'Invalid PhoneNumber';
+
+    return null;
   }
 }
